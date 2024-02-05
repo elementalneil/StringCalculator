7b253c1e9dd97c6e95fc3d5c300ab3ed375cbb66
Sankhanil Nayek <nayek.sankhanil@gmail.com>
2024-02-05 15:00:44 +0530

    Updated Library

--- StringCalculator/StringCalculatorLib/StringCalculator.cs
+++ StringCalculator/StringCalculatorLib/StringCalculator.cs
@@ -7,22 +7,28 @@ using System.Threading.Tasks;
 
 namespace StringCalculatorLib {
     public static class StringCalculator {
+        public static string[] GenerateCustomDelimiters(string numbers) {
+            string[] customDelimiters = null;
+            if (numbers.StartsWith("//")) {
+                var customDelimiterMatch = Regex.Match(numbers, @"//(.+?)\n");
+                if (customDelimiterMatch.Success) {
+                    var customDelimiter = customDelimiterMatch.Groups[1].Value;
+                    customDelimiters = new[] { customDelimiter };
+                }
+            }
+
+            return customDelimiters;
+        }
+
         public static List<int> GenerateNumbersArray(string numbers) {
             // Default delimiters: comma and newline
             string[] delimiters = { ",", "\n" };
 
             // Check for custom delimiter format
-            if (numbers.StartsWith("//")) {
-                var customDelimiterMatch = Regex.Match(numbers, @"//(.+?)\n");
-                if (customDelimiterMatch.Success) {
-                    var customDelimiter = customDelimiterMatch.Groups[1].Value;
-                    delimiters = new[] { customDelimiter };
-
-                    numbers = numbers.Substring(customDelimiterMatch.Length);
-                }
-            }
-            else {
-
+            string[] customDelimiters = GenerateCustomDelimiters(numbers);
+            if (customDelimiters != null) {
+                delimiters = customDelimiters;
+                numbers = numbers.Substring(numbers.IndexOf("\n") + 1);
             }
 
             List<int> numbersArray;
