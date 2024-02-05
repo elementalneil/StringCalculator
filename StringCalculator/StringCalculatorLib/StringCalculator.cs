983fb16bff6ece902a02a50d4f03f0c9072c38da
Sankhanil Nayek <nayek.sankhanil@gmail.com>
2024-02-05 14:33:14 +0530

    Modified StringCalculator

--- StringCalculator/StringCalculatorLib/StringCalculator.cs
+++ StringCalculator/StringCalculatorLib/StringCalculator.cs
@@ -7,24 +7,21 @@ using System.Threading.Tasks;
 
 namespace StringCalculatorLib {
     public static class StringCalculator {
-        public static int Add(string numbers) {
-            if (string.IsNullOrEmpty(numbers))
-                return 0;
-            
+        public static List<int> GenerateNumbersArray(string numbers) {
             // Default delimiters: comma and newline
             string[] delimiters = { ",", "\n" };
-            
+
             // Check for custom delimiter format
             if (numbers.StartsWith("//")) {
                 var customDelimiterMatch = Regex.Match(numbers, @"//(.+?)\n");
                 if (customDelimiterMatch.Success) {
                     var customDelimiter = customDelimiterMatch.Groups[1].Value;
                     delimiters = new[] { customDelimiter };
-            
+
                     numbers = numbers.Substring(customDelimiterMatch.Length);
                 }
             }
-            
+
             List<int> numbersArray;
             try {
                 numbersArray = numbers.Split(delimiters, StringSplitOptions.RemoveEmptyEntries)
@@ -34,16 +31,25 @@ namespace StringCalculatorLib {
             catch {
                 throw new ArgumentException("Invalid Input");
             }
-            
+
+            return numbersArray;
+        }
+
+        public static int Add(string numbers) {
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+
+            List<int> numbersArray = GenerateNumbersArray(numbers);
+
             // Check for negative numbers and throw an exception
             var negativeNumbers = numbersArray.Where(n => n < 0).ToList();
             if (negativeNumbers.Any()) {
                 throw new ArgumentException($"Negatives not allowed: {string.Join(", ", negativeNumbers)}");
             }
-            
+
             // Ignore numbers greater than 1000
             numbersArray = numbersArray.Where(n => n <= 1000).ToList();
-            
+
             // Return the sum of the numbers
             return numbersArray.Sum();
         }
